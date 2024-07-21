b2e59c86d90e3acd7b46afa37183f2267b022eae
root <johnkalalak@gmail.com>
2024-07-21 20:10:01 +0200

    All added

---
+++ src/main/java/dev/jk/demoawscicd/DemoAwsCicdApplication.java
@@ -0,0 +1,13 @@
+package dev.jk.demoawscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jk/demoawscicd/DemoAwsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jk.demoawscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAwsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
